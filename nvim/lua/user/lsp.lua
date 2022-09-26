307b497f0f3ad7c210a48ddbc21160eb1786a193
nilventosa <ventosa@gmail.com>
2022-09-26 15:10:00 +0200

    Add more rust LSP bindings

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -1,7 +1,20 @@
 -- LSP
+-- 
+-- -- RUST
 
 require 'lspconfig'.rust_analyzer.setup{
     on_attach = function()
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<CR>", {})
+        -- lsp stuff
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+        vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, {buffer=0})
+        vim.keymap.set("n", "gI", vim.lsp.buf.implementation, {buffer=0})
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {buffer=0})
+        -- diagnostics stuff
+        vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0})
+        vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0})
+        vim.keymap.set("n", "<leader>D", vim.diagnostic.open_float, {buffer=0})
+        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<CR>", {})
     end,
 }
